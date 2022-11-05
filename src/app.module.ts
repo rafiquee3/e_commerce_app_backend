8ada6e56b215975d02dc1aa6d74a8322d188cd95
Rafal Sokolowski <rafiquee.code@gmail.com>
2022-11-05 19:58:06 +0100

    Feat: products module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ProductsController } from './products.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
+  imports: [ProductsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
