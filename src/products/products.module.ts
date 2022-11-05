4beb4f4b346e8ca3e1e47bd00ed6284b70bc580b
Rafal Sokolowski <rafiquee.code@gmail.com>
2022-11-05 20:26:41 +0100

    Feat: logger.middleware

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-
+// to provide a set of providers which should be available everywhere out-of-the-box (e.g., helpers, database connections, etc.)
+//@Global() //imports array is generally the preferred way to make the module's API available to consumers.
 @Module({
   imports: [],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
 })
-export class ProductsModule {}
+// A module class can inject providers as well (e.g., for configuration purposes):
+export class ProductsModule {
+  constructor(private productsService: ProductsService) {}
+}
